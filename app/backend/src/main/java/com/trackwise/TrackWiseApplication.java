7a01afea36807fda32b25158d0abd386537b547b
eduardofreitasf <eduardof.fernandes05@gmail.com>
2026-07-24 21:47:41 +0100

    feat(backend): implement auditable BaseEntity and enable JPA auditing

--- app/backend/src/main/java/com/trackwise/TrackWiseApplication.java
+++ app/backend/src/main/java/com/trackwise/TrackWiseApplication.java
@@ -2,8 +2,10 @@ package com.trackwise;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class TrackWiseApplication {
 
 	public static void main(String[] args) {
